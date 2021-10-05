697cf798fef8ab73a4c4f4d8ee180c7f565e0a5b
Chanhee Cho <chanhee@chanhees-mbp.wifi.local.cmu.edu>
2021-10-05 15:13:37 -0400

    test

--- write_file.py
+++ write_file.py
@@ -1,3 +1,7 @@
-f = open("demofile.txt", "a")
+import os
+
+directory = "./demofolder"
+os.mkdir(directory)
+f = open("./demofolder/demofile.txt", "a")
 f.writelines(["\nSee you soon!", "\nOver and out."])
 f.close()
